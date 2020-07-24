c96f101ceedd434d485f73526b00b69cac6fd3f6
edi <plataformaedi@gmail.com>
2020-07-24 17:11:09 -0300

    commit calculadora edi

---
+++ Calculadora-25-07-2020/EDI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Digite o 1 valor");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+            
+         
+
+            Console.WriteLine("Digite a operação +-/*");
+            string operação = Console.ReadLine();
+
+        Console.WriteLine("Digite o 2 valor");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+            
+            double resultado=0;
+            if (operação == "+")
+                resultado = valor1+valor2;
+
+            if (operação == "-")
+                resultado = valor1-valor2;
+
+             if (operação == "*")
+                resultado = valor1*valor2;
+
+
+             if (operação == "/")
+                resultado = valor1/valor2; 
+
+                Console.WriteLine(resultado);
+
+                
+        
+             
+        
+                
+            
+        
+            
+
+
+
+
+    
+        }
+    }
+}
