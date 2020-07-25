5d65ee8fdb8bb6d1571e37d4f3ba106f520bd50e
marcos010894 <marcospaulo.dev@hotmail.com>
2020-07-25 17:08:56 -0300

    Medir imc, commit marcos

---
+++ IMC-25-07-2020/marcos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //simples calculadora
+
+            float A, P, imc;
+
+              Console.Write("Digite o seu peso: ex 90");
+            P = float.Parse(Console.ReadLine());
+
+
+            Console.Write("Digite o seu altura, EX 1,70:");
+            A =  float.Parse(Console.ReadLine());
+
+            imc = (P / (A * A));
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Peso abaixo do normal");
+            }
+            else if ((imc > 18.5) && (imc < 25))
+            {
+                Console.WriteLine("Peso normal");
+            }
+            else  if ((imc >= 25) && (imc < 30))
+            {
+                Console.WriteLine("Sobre o Peso");
+            }
+            else if ((imc > 30))
+            {
+                Console.WriteLine("Cara Voce esta obeso");
+            }
+            //sou foda
+
+            }
+        }
+    }
